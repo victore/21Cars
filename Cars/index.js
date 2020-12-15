b7710f9736a22ff9a195df20c33a7c16ffd3e13b
Victor Guadarrama <victor.guadarrama3@gmail.com>
2020-12-15 11:52:50 -0600

    Add cars catalog

---
+++ Cars/index.js
@@ -0,0 +1,58 @@
+const cars = [
+  {
+    id: 1,
+    name: "GTI 2020",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti.jpg",
+  },
+  {
+    id: 2,
+    name: "GTI 2019",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti3.jpg",
+  },
+  {
+    id: 3,
+    name: "GTI 2021",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti2.jpg",
+  },
+  {
+    id: 4,
+    name: "Car 4",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti2.jpg",
+  },
+  {
+    id: 5,
+    name: "Car 5",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti4.jpg",
+  },
+  {
+    id: 6,
+    name: "Car 6",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti.jpg",
+  },
+  {
+    id: 7,
+    name: "Car 7",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti4.jpg",
+  },
+  {
+    id: 8,
+    name: "Car 8",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti3.jpg",
+  },
+  {
+    id: 9,
+    name: "Car 9",
+    spec: "Here the specs of the current car.",
+    image: "https://21cars.s3-us-west-2.amazonaws.com/gti4.jpg",
+  },
+];
+
+export default cars;
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["21cars.s3-us-west-2.amazonaws.com"],
+  },
+};
